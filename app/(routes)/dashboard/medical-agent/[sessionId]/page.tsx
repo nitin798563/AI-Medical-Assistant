05c99d25325f7c6b0dfa8d9eedd684f712697e57
nitin798563 <ansingh6388@gmail.com>
2025-11-14 22:53:22 +0530

    Add Vapi

--- app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -3,53 +3,96 @@ import axios from 'axios';
 import { useParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import type { doctorAgent } from '../../_component/DoctorAgentCard';
-import { Circle, PhoneCall } from 'lucide-react';
+import { Circle, PhoneCall, PhoneOff } from 'lucide-react';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import Vapi from '@vapi-ai/web';
 
 
-type SessionDetail={
-  id:number,
-  notes:string,
+type SessionDetail = {
+  id: number,
+  notes: string,
   sessionId: string,
-  report:JSON,
-  selectedDoctor:doctorAgent,
+  report: JSON,
+  selectedDoctor: doctorAgent,
   createdOn: string
 }
 
 function MedicalVoiceAgent() {
 
-const {sessionId} = useParams();
-const [sessionDetail, setSessionDetail] = useState<SessionDetail>();
+  const { sessionId } = useParams();
+  const [sessionDetail, setSessionDetail] = useState<SessionDetail>();
+  const [callStarted, setCallStarted] = useState(false);
+  const [vapiInstance, setVapiInstance] = useState<any>();
 
-useEffect(() => {
-  sessionId && GetSessionDetails();
-}, [sessionId]);
 
-const GetSessionDetails = async () => {
-  const result = await axios.get('/api/session-chat?sessionId='+sessionId);
-  console.log(result.data);
-  setSessionDetail(result.data)
-}
+  useEffect(() => {
+    sessionId && GetSessionDetails();
+  }, [sessionId]);
+
+  const GetSessionDetails = async () => {
+    const result = await axios.get('/api/session-chat?sessionId=' + sessionId);
+    console.log(result.data);
+    setSessionDetail(result.data)
+  }
+
+  const StartCall = () => {
+    const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
+    setVapiInstance(vapi)
+    vapi.start(process.env.NEXT_PUBLIC_VAPI_VOICE_ASSISTANT_ID);
+    vapi.on('call-start', () => {
+      console.log('Call started')
+      setCallStarted(true);
+    });
+    vapi.on('call-end', () => {
+      setCallStarted(false);
+      console.log('Call ended')
+    });
+    vapi.on('message', (message) => {
+      if (message.type === 'transcript') {
+        console.log(`${message.role}: ${message.transcript}`);
+      }
+    });
+  }
+
+  const endCall = () => {
+    if(!vapiInstance) return;
+    console.log("Ending Call....")
+
+    //Stop the call
+    vapiInstance.stop();
+
+
+    //optional remove listeners (good for every managemnet)
+    vapiInstance.off("call-start");
+    vapiInstance.off("call-end");
+    vapiInstance.off("message")
+
+    //Reset call state
+    setCallStarted(false);
+    setVapiInstance(null);
+
+  };
 
   return (
     <div className='p-5 border rounded-3xl bg-secondary'>
-    <div className='flex justify-between items-center'>
-      <h2 className='p-1 px-2 border rounded-md flex gap-2 items-center'><Circle className='h-4 w-4'/>Not Connected</h2>
-      <h2 className='font-bold text-xl text-gray-400'>00:00</h2>
+      <div className='flex justify-between items-center'>
+        <h2 className='p-1 px-2 border rounded-md flex gap-2 items-center'><Circle className={`h-4 w-4 rounded-full ${callStarted ? "bg-green-500" : "bg-red-500"}`} />{callStarted ? "Connected..." : "Not Connected"}</h2>
+        <h2 className='font-bold text-xl text-gray-400'>00:00</h2>
       </div>
-      {sessionDetail&&<div className='flex items-center flex-col mt-10'>
-        <Image src={sessionDetail?.selectedDoctor?.image} alt={sessionDetail?.selectedDoctor?.specialist??""}
-        width={120}
-        height={120}
-        className='h-[100px] w-[100px] object-cover rounded-full '/>
+      {sessionDetail && <div className='flex items-center flex-col mt-10'>
+        <Image src={sessionDetail?.selectedDoctor?.image} alt={sessionDetail?.selectedDoctor?.specialist ?? ""}
+          width={120}
+          height={120}
+          className='h-[100px] w-[100px] object-cover rounded-full ' />
         <h2 className='mt-2 text-lg '>{sessionDetail?.selectedDoctor?.specialist}</h2>
         <p className='text-sm text-gray-400'>AI Medical Voice Agent</p>
         <div className='mt-32'>
           <h2 className='text-gray-400'>Assistant Msg</h2>
           <h2 className='text-lg'>User Msg</h2>
         </div>
-        <Button className='mt-20'><PhoneCall/>Start Call</Button>
+        {!callStarted ? <Button className='mt-20' onClick={StartCall}><PhoneCall />Start Call</Button> :
+          <Button variant={"destructive"} onClick={endCall}> <PhoneOff />Disconnect</Button>}
       </div>}
     </div>
   )
